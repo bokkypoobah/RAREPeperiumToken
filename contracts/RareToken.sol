bfa430ed35c1dc3194ec86fa1c10f63b7dd76844
BokkyPooBah <bokky.poobah@bokconsulting.com.au>
2017-04-25 02:01:29 +1000

    Fixed issues: * #1 Remove the mintToken(...) function * #2 Fix the totalSupply issue * #3 Remove freezeAccount(...) function * #4 Remove approveAndCall(...) function * #5 Remove buy(), sell(...), setPrices(...) functions
    Also renamed deflateToken(target, mintedAmount) to burnTokens(amount),
    and the burnt amount only comes out of the owner's token balance.

--- contracts/RareToken.sol
+++ contracts/RareToken.sol
@@ -2,6 +2,7 @@ pragma solidity ^0.4.8;
 
 contract Owned {
     address public owner;
+    event OwnershipTransferred(address indexed _from, address indexed _to);
 
     function Owned() {
         owner = msg.sender;
@@ -13,6 +14,7 @@ contract Owned {
     }
 
     function transferOwnership(address newOwner) onlyOwner {
+        OwnershipTransferred(owner, newOwner);
         owner = newOwner;
     }
 }
@@ -98,90 +100,19 @@ contract Token {
 }
 
 contract RareToken is Owned, Token {
-
-    uint256 public sellPrice;
-    uint256 public buyPrice;
-    uint256 public totalSupply;
-
-    mapping (address => bool) public frozenAccount;
-
-    /* This generates a public event on the blockchain that will notify clients */
-    event FrozenFunds(address target, bool frozen);
-
     /* Initializes contract with initial supply tokens to the creator of the contract */
     function RareToken(
         uint256 initialSupply,
         string tokenName,
         uint8 decimalUnits,
-        string tokenSymbol,
-        address centralMinter
+        string tokenSymbol
     ) Token (initialSupply, tokenName, decimalUnits, tokenSymbol) {
-        if (centralMinter != 0 ) owner = centralMinter;      // Sets the owner as specified (if centralMinter is not specified the owner is msg.sender)
-        balanceOf[owner] = initialSupply;                   // Give the owner all initial tokens
     }
 
-    /* Send coins */
-    function transfer(address _to, uint256 _value) {
-        if (balanceOf[msg.sender] < _value) throw;           // Check if the sender has enough
-        if (balanceOf[_to] + _value < balanceOf[_to]) throw; // Check for overflows
-        if (frozenAccount[msg.sender]) throw;                // Check if frozen
-        balanceOf[msg.sender] -= _value;                     // Subtract from the sender
-        balanceOf[_to] += _value;                            // Add the same to the recipient
-        Transfer(msg.sender, _to, _value);                   // Notify anyone listening that this transfer took place
-    }
-
-    /* A contract attempts to get the coins */
-    function transferFrom(address _from, address _to, uint256 _value) returns (bool success) {
-        if (frozenAccount[_from]) throw;                        // Check if frozen
-        if (balanceOf[_from] < _value) throw;                 // Check if the sender has enough
-        if (balanceOf[_to] + _value < balanceOf[_to]) throw;  // Check for overflows
-        if (_value > allowance[_from][msg.sender]) throw;   // Check allowance
-        balanceOf[_from] -= _value;                          // Subtract from the sender
-        balanceOf[_to] += _value;                            // Add the same to the recipient
-        allowance[_from][msg.sender] -= _value;
-        Transfer(_from, _to, _value);
-        return true;
-    }
-
-    function mintToken(address target, uint256 mintedAmount) onlyOwner {
-        balanceOf[target] += mintedAmount;
-        totalSupply += mintedAmount;
-        Transfer(0, this, mintedAmount);
-        Transfer(this, target, mintedAmount);
-    }
-
-    function deflateToken(address target, uint256 mintedAmount) onlyOwner {
-        balanceOf[target] -= mintedAmount;
-        totalSupply -= mintedAmount;
-        Transfer(target, 0, mintedAmount);
-    }
-
-    function freezeAccount(address target, bool freeze) onlyOwner {
-        frozenAccount[target] = freeze;
-        FrozenFunds(target, freeze);
-    }
-
-    function setPrices(uint256 newSellPrice, uint256 newBuyPrice) onlyOwner {
-        sellPrice = newSellPrice;
-        buyPrice = newBuyPrice;
-    }
-
-    function buy() payable {
-        uint amount = msg.value / buyPrice;                // calculates the amount
-        if (balanceOf[this] < amount) throw;               // checks if it has enough to sell
-        balanceOf[msg.sender] += amount;                   // adds the amount to buyer's balance
-        balanceOf[this] -= amount;                         // subtracts amount from seller's balance
-        Transfer(this, msg.sender, amount);                // execute an event reflecting the change
-    }
-
-    function sell(uint256 amount) {
-        if (balanceOf[msg.sender] < amount ) throw;        // checks if the sender has enough to sell
-        balanceOf[this] += amount;                         // adds the amount to owner's balance
-        balanceOf[msg.sender] -= amount;                   // subtracts the amount from seller's balance
-        if (!msg.sender.send(amount * sellPrice)) {        // sends ether to the seller. It's important
-            throw;                                         // to do this last to avoid recursion attacks
-        } else {
-            Transfer(msg.sender, this, amount);            // executes an event reflecting on the change
-        }
+    function burnTokens(uint256 _value) onlyOwner {
+        if (balanceOf[owner] < _value) throw;       // Check if the sender has enough
+        balanceOf[owner] -= _value;
+        totalSupply -= _value;
+        Transfer(target, 0, _value);
     }
 }
