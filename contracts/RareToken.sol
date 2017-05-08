99b0cc0f36ea2f25679296f5b82ed6dbf0ebe3d3
BokkyPooBah <bokky.poobah@bokconsulting.com.au>
2017-05-08 20:58:38 +1000

    Update. Removed approveAndCall(...)

--- contracts/RareToken.sol
+++ contracts/RareToken.sol
@@ -27,92 +27,102 @@ contract Owned {
 }
 
 
-contract TokenRecipient { 
-    function receiveApproval(address _from, uint256 _value, address _token, bytes _extraData);
-}
-
-
-contract Token {
-    /* Public variables of the token */
-    string public standard = 'Token 0.1';
-    string public name;
+contract ERC20Token is Owned {
     string public symbol;
+    string public name;
     uint8 public decimals;
     uint256 public totalSupply;
 
-    /* This creates an array with all balances */
-    mapping (address => uint256) public balanceOf;
-    mapping (address => mapping (address => uint256)) public allowance;
+    // Balances for each account
+    mapping (address => uint256) balances;
+    
+    // Owner of account approves the transfer of an amount to another account
+    mapping (address => mapping (address => uint256)) allowed;
 
-    /* This generates a public event on the blockchain that will notify clients */
+    // Triggered when tokens are transferred.
     event Transfer(address indexed from, address indexed to, uint256 value);
 
-    /* Initializes contract with initial supply tokens to the creator of the contract */
-    function Token(
-        uint256 initialSupply,
-        string tokenName,
-        uint8 decimalUnits,
-        string tokenSymbol
-    ) {
-        balanceOf[msg.sender] = initialSupply;              // Give the creator all initial tokens
-        totalSupply = initialSupply;                        // Update total supply
-        name = tokenName;                                   // Set the name for display purposes
-        symbol = tokenSymbol;                               // Set the symbol for display purposes
-        decimals = decimalUnits;                            // Amount of decimals for display purposes
+    // Triggered whenever approve(address _spender, uint256 _value) is called.
+    event Approval(address indexed _owner, address indexed _spender, uint256 _value);
+
+    // Constructor
+    function ERC20Token(string _symbol, string _name, uint8 _decimals, uint256 _totalSupply) {
+        symbol = _symbol;
+        name = _name;
+        decimals = _decimals;
+        totalSupply = _totalSupply;
+        balances[msg.sender] = _totalSupply;
     }
 
-    /* Send coins */
-    function transfer(address _to, uint256 _value) {
-        if (balanceOf[msg.sender] < _value) throw;           // Check if the sender has enough
-        if (balanceOf[_to] + _value < balanceOf[_to]) throw; // Check for overflows
-        balanceOf[msg.sender] -= _value;                     // Subtract from the sender
-        balanceOf[_to] += _value;                            // Add the same to the recipient
-        Transfer(msg.sender, _to, _value);                   // Notify anyone listening that this transfer took place
+    // What is the balance of a particular account?
+    function balanceOf(address _owner) constant returns (uint256 balance) {
+        return balances[_owner];
     }
 
-    /* Allow another contract to spend some tokens in your behalf */
-    function approve(address _spender, uint256 _value)
-        returns (bool success) {
-        allowance[msg.sender][_spender] = _value;
-        TokenRecipient spender = TokenRecipient(_spender);
-        return true;
-    }
-
-    /* Approve and then comunicate the approved contract in a single tx */
-    function approveAndCall(address _spender, uint256 _value, bytes _extraData)
-        returns (bool success) {
-        TokenRecipient spender = TokenRecipient(_spender);
-        if (approve(_spender, _value)) {
-            spender.receiveApproval(msg.sender, _value, this, _extraData);
+    // Transfer the balance from owner's account to another account
+    function transfer(address _to, uint256 _amount) returns (bool success) {
+        if (balances[msg.sender] >= _amount
+            && _amount > 0
+            && balances[_to] + _amount > balances[_to]) {
+            balances[msg.sender] -= _amount;
+            balances[_to] += _amount;
+            Transfer(msg.sender, _to, _amount);
             return true;
+        } else {
+            return false;
         }
     }
 
-    /* A contract attempts to get the coins */
-    function transferFrom(address _from, address _to, uint256 _value) returns (bool success) {
-        if (balanceOf[_from] < _value) throw;                // Check if the sender has enough
-        if (balanceOf[_to] + _value < balanceOf[_to]) throw; // Check for overflows
-        if (_value > allowance[_from][msg.sender]) throw;    // Check allowance
-        balanceOf[_from] -= _value;                          // Subtract from the sender
-        balanceOf[_to] += _value;                            // Add the same to the recipient
-        allowance[_from][msg.sender] -= _value;
-        Transfer(_from, _to, _value);
+    // Send _value amount of tokens from address _from to address _to
+    // The sender of the message will need to have had the spending approved
+    // via the approve(...) function
+    function transferFrom(
+        address _from,
+        address _to,
+        uint256 _amount
+    ) returns (bool success) {
+        if (balances[_from] >= _amount
+            && allowed[_from][msg.sender] >= _amount
+            && _amount > 0
+            && balances[_to] + _amount > balances[_to]) {
+            balances[_from] -= _amount;
+            allowed[_from][msg.sender] -= _amount;
+            balances[_to] += _amount;
+            Transfer(_from, _to, _amount);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    // Allow _spender to withdraw from your account, multiple times, up to the _value amount.
+    // If this function is called again it overwrites the current allowance with _value.
+    function approve(address _spender, uint256 _amount) returns (bool success) {
+        allowed[msg.sender][_spender] = _amount;
+        Approval(msg.sender, _spender, _amount);
         return true;
     }
 
-    /* This unnamed function is called whenever someone tries to send ether to it */
+    // Returns the amount which _spender is still allowed to withdraw from _owner
+    function allowance(address _owner, address _spender) constant returns (uint256 remaining) {
+        return allowed[_owner][_spender];
+    }
+    
+    // Don't accept ethers
     function () {
-        throw;                                               // Prevents accidental sending of ether
+        throw;
     }
 }
 
-contract RareToken is Owned, Token {
+
+contract RareToken is ERC20Token {
     /* 100,000,000 tokens, 8 decimal places */
-    function RareToken() Token (10000000000000000, "RARE", 8, "R$") {}
+    function RareToken() ERC20Token ("RARE", "RARE", 8, 10000000000000000) {
+    }
 
     function burnTokens(uint256 _value) onlyOwner {
-        if (balanceOf[owner] < _value) throw;                // Check if the sender has enough
-        balanceOf[owner] -= _value;
+        if (balances[owner] < _value) throw;
+        balances[owner] -= _value;
         totalSupply -= _value;
         Transfer(owner, 0, _value);
     }
