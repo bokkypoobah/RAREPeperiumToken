55d1b25b3303c92d766c781ca083d45dfc21c290
BokkyPooBah <bokky.poobah@bokconsulting.com.au>
2017-06-07 20:50:34 +1000

    Test completed

--- contracts/RareToken.sol
+++ contracts/RareToken.sol
@@ -3,7 +3,9 @@ pragma solidity ^0.4.9;
 // ----------------------------------------------------------------------------------------------
 // The new RARE token contract
 //
-// Enjoy. (c) Michael C and BokkyPooBah / Bok Consulting Pty Ltd 2017. The MIT Licence.
+// https://github.com/bokkypoobah/RAREPeperiumToken
+//
+// Enjoy. (c) BokkyPooBah / Bok Consulting Pty Ltd 2017 for Michael C. The MIT Licence.
 // ----------------------------------------------------------------------------------------------
 
 contract Owned {
@@ -39,19 +41,25 @@ contract ERC20Token is Owned {
     uint8 public decimals;
     uint256 public totalSupply;
 
+    // ------------------------------------------------------------------------
     // Balances for each account
+    // ------------------------------------------------------------------------
     mapping (address => uint256) balances;
-    
+
+    // ------------------------------------------------------------------------
     // Owner of account approves the transfer of an amount to another account
+    // ------------------------------------------------------------------------
     mapping (address => mapping (address => uint256)) allowed;
 
-    // Triggered when tokens are transferred.
+    // ------------------------------------------------------------------------
+    // Events
+    // ------------------------------------------------------------------------
     event Transfer(address indexed from, address indexed to, uint256 value);
-
-    // Triggered whenever approve(address _spender, uint256 _value) is called.
     event Approval(address indexed _owner, address indexed _spender, uint256 _value);
 
+    // ------------------------------------------------------------------------
     // Constructor
+    // ------------------------------------------------------------------------
     function ERC20Token(string _symbol, string _name, uint8 _decimals, uint256 _totalSupply) {
         symbol = _symbol;
         name = _name;
@@ -60,16 +68,21 @@ contract ERC20Token is Owned {
         balances[msg.sender] = _totalSupply;
     }
 
-    // What is the balance of a particular account?
+    // ------------------------------------------------------------------------
+    // Get the account balance of another account with address _owner
+    // ------------------------------------------------------------------------
     function balanceOf(address _owner) constant returns (uint256 balance) {
         return balances[_owner];
     }
 
+    // ------------------------------------------------------------------------
     // Transfer the balance from owner's account to another account
+    // ------------------------------------------------------------------------
     function transfer(address _to, uint256 _amount) returns (bool success) {
-        if (balances[msg.sender] >= _amount
-            && _amount > 0
-            && balances[_to] + _amount > balances[_to]) {
+        if (balances[msg.sender] >= _amount             // User has balance
+            && _amount > 0                              // Non-zero transfer
+            && balances[_to] + _amount > balances[_to]  // Overflow check
+        ) {
             balances[msg.sender] -= _amount;
             balances[_to] += _amount;
             Transfer(msg.sender, _to, _amount);
@@ -79,18 +92,32 @@ contract ERC20Token is Owned {
         }
     }
 
-    // Send _value amount of tokens from address _from to address _to
-    // The sender of the message will need to have had the spending approved
-    // via the approve(...) function
+    // ------------------------------------------------------------------------
+    // Allow _spender to withdraw from your account, multiple times, up to the
+    // _value amount. If this function is called again it overwrites the
+    // current allowance with _value.
+    // ------------------------------------------------------------------------
+    function approve(address _spender, uint256 _amount) returns (bool success) {
+        allowed[msg.sender][_spender] = _amount;
+        Approval(msg.sender, _spender, _amount);
+        return true;
+    }
+
+    // ------------------------------------------------------------------------
+    // Spender of tokens transfer an amount of tokens from the token owner's
+    // balance to another account. The owner of the tokens must already
+    // have approve(...)-d this transfer
+    // ------------------------------------------------------------------------
     function transferFrom(
         address _from,
         address _to,
         uint256 _amount
     ) returns (bool success) {
-        if (balances[_from] >= _amount
-            && allowed[_from][msg.sender] >= _amount
-            && _amount > 0
-            && balances[_to] + _amount > balances[_to]) {
+        if (balances[_from] >= _amount                  // From a/c has balance
+            && allowed[_from][msg.sender] >= _amount    // Transfer approved
+            && _amount > 0                              // Non-zero transfer
+            && balances[_to] + _amount > balances[_to]  // Overflow check
+        ) {
             balances[_from] -= _amount;
             allowed[_from][msg.sender] -= _amount;
             balances[_to] += _amount;
@@ -101,20 +128,17 @@ contract ERC20Token is Owned {
         }
     }
 
-    // Allow _spender to withdraw from your account, multiple times, up to the _value amount.
-    // If this function is called again it overwrites the current allowance with _value.
-    function approve(address _spender, uint256 _amount) returns (bool success) {
-        allowed[msg.sender][_spender] = _amount;
-        Approval(msg.sender, _spender, _amount);
-        return true;
-    }
-
-    // Returns the amount which _spender is still allowed to withdraw from _owner
+    // ------------------------------------------------------------------------
+    // Returns the amount of tokens approved by the owner that can be
+    // transferred to the spender's account
+    // ------------------------------------------------------------------------
     function allowance(address _owner, address _spender) constant returns (uint256 remaining) {
         return allowed[_owner][_spender];
     }
-    
+
+    // ------------------------------------------------------------------------
     // Don't accept ethers
+    // ------------------------------------------------------------------------
     function () {
         throw;
     }
@@ -122,8 +146,10 @@ contract ERC20Token is Owned {
 
 
 contract RareToken is ERC20Token {
-    /* 100,000,000 tokens that will be populated by the fill, 8 decimal places */
-    function RareToken() ERC20Token ("RARE", "RARE", 8, 0) {
+    // ------------------------------------------------------------------------
+    // 100,000,000 tokens that will be populated by the fill, 8 decimal places
+    // ------------------------------------------------------------------------
+    function RareToken() ERC20Token ("RARE", "RARE", 8, 10**16) {
     }
 
     function burnTokens(uint256 value) onlyOwner {
@@ -133,6 +159,9 @@ contract RareToken is ERC20Token {
         Transfer(owner, 0, value);
     }
 
+    // ------------------------------------------------------------------------
+    // Fill - to populate tokens from the old token contract
+    // ------------------------------------------------------------------------
     // From https://github.com/BitySA/whetcwithdraw/tree/master/daobalance
     bool public sealed;
     // The compiler will warn that this constant does not match the address checksum
@@ -153,6 +182,9 @@ contract RareToken is ERC20Token {
         }
     }
 
+    // ------------------------------------------------------------------------
+    // After sealing, no more filling is possible
+    // ------------------------------------------------------------------------
     function seal() onlyOwner {
         if (sealed) throw;    
         sealed = true;
